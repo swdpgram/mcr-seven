028c5eb071175f4d546b265be72e6e4a6fae8bed
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-07-09 22:18:20 +0530

    continent page

--- src/ContinentCard.jsx
+++ src/ContinentCard.jsx
@@ -1,41 +1,27 @@
 import { useState, useEffect } from "react"; 
 import { Link } from "react-router-dom";
+import { Card } from "./Card";
 export function ContinentCard({ 
 countries,
 name,
 image
 }) { 
 
-const [ backgroundImage, setBackgroundImage ] = useState("")
 
-
-useEffect(()=> { 
-const updateImage = () => { 
-    setBackgroundImage(image)
-}
-
-updateImage()
-},[image])
 
     return(
-        <Link to={"/destinations/"+name}>
-        <div 
-        style={{backgroundImage: `url(${backgroundImage})`}}
-        className="card"
-        > 
-        
-        </div>
 
-        <div className="names-icons"> 
-        <i
-              className="material-symbols-outlined"
-            //   onClick={() =>deleteRecipe(id)}
-            >
-              location_on
-            </i>
-          
-        <span> {name} </span>     
+        <div className="home-tiles continent-page"> 
+        { 
+        countries.map((item,index)=> { 
+            return(
+                <div key={index} >
+                    <Card {...item} /> 
+                </div>
+            );
+        })
+        }
         </div>
-        </Link>
+        
     ); 
 }
--- src/ContinentPage.jsx
+++ src/ContinentPage.jsx
@@ -1,50 +1,50 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { data } from "./data"
-import { ContinentCard } from "./ContinentCard"
-export function ContinentPage() { 
+import { data } from "./data";
+import { ContinentCard } from "./ContinentCard";
+export function ContinentPage() {
+  const { name } = useParams();
 
-    const { name } = useParams(); 
+  useEffect(() => {
+    const continentData = () => {
+      const individualContinentData = data.continents.filter(
+        (continent) => continent.name === name
+      );
+      console.log(individualContinentData);
+      setCountries(individualContinentData);
+    };
 
-   useEffect(()=> { 
+    continentData();
+  }, [name]);
 
-     const continentData = () => { 
-       const individualContinentData = data.continents.filter((continent)=>continent.name===name)
-       console.log(individualContinentData)
-    setCountries(individualContinentData)
-     }
+  const [countries, setCountries] = useState([]);
 
-     continentData(); 
-   },[name]); 
-
-   const [ countries, setCountries] = useState([]); 
-
-    return (
-        <>
-        <Link to="/"> 
+  return (
+    <>
+      <Link to="/" className="link-to-home">
+        
         <i
-              className="material-symbols-outlined"
-              id="home-icon"
-            //   onClick={() =>deleteRecipe(id)}
-            >
-              home
-            </i>
-        Go to Home 
-        </Link>
+          className="material-symbols-outlined"
+          id="home-icon"
+          //   onClick={() =>deleteRecipe(id)}
+        >
+          home
+        </i>
+        <span> Go to Home </span>
+      </Link>
 
-        Continent Page for {name}
+      <div className="continentpage-container">
+        <p className="title"> Top Countries in {name} for your next holiday </p>
 
-    { 
-    countries.map((item,index)=> { 
-        return(
+        {countries.map((item, index) => {
+          return (
             <div key={index}>
-                <ContinentCard {...item} /> 
+              <ContinentCard {...item} />
             </div>
-        ); 
-    })
-    }
-
-        </>
-    ); 
+          );
+        })}
+      </div>
+    </>
+  );
 }
